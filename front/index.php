7d8b146509070793127eb3c4da90fab0ee66aac6
tmorio <morikapusan@eleken.jp>
2018-10-20 16:53:22 +0900

    Update CSS

--- front/index.php
+++ front/index.php
@@ -2,24 +2,80 @@
 
 <html lang="ja">
 <head>
-  <meta charset="utf-8">
-  <title>Design Test</title>
-  <link rel="stylesheet" href="styles.css">
+	<meta charset="utf-8">
+	<title>Design Test</title>
+	<link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
 
 <div class="title">
-        #koryosai2018
+	<img border="0" src="./twitterlogo.png">&thinsp;&thinsp;<span class="title">#koryosai2018</span>
 </div>
+
+<br>
 
 <div class="container">
-  <div class="item box-big1">Content with Media</div>
-  <div class="item1">Content Text1</div>
-  <div class="item2">Content Text2</div>
-  <div class="item3">Content Text3</div>
-  <div class="item4">Content Text4</div>
+  <div class="photo">
+	<div class="bigarea">
+		<img src="./pic.jpg" class="bigpic">
+	</div>
+	<div class="area">
+		<img src="./icon.jpg">&nbsp;もりかぷ @InaSMori
+	</div>
+	<br>
+	<div class="textarea">
+		<div class="marquee">
+			<div class="marquee-inner">それっぽくなってきた！</div>
+		</div>
+	</div>
+  </div>
+
+  <div class="item1">
+        <div class="areasub">
+                <img src="./icon.jpg">&nbsp;もりかぷ @InaSMori
+        </div>
+        <div class="textareapink">
+                <div class="marquee">
+                        <div class="marquee-inner">PHPはいいぞ。</div>
+                </div>
+	</div>
 </div>
+
+  <div class="item2">
+        <div class="areasub">
+                <img src="./icon.jpg">&nbsp;もりかぷ @InaSMori
+        </div>
+        <div class="textareagreen">
+                <div class="marquee">
+                        <div class="marquee-inner">イナズマイレブンのゲームはいつ出るのやら...</div>
+                </div>
+        </div>
+</div>
+
+  <div class="item3">
+        <div class="areasub">
+                <img src="./icon.jpg">&nbsp;もりかぷ @InaSMori
+        </div>
+        <div class="textareablue">
+                <div class="marquee">
+                        <div class="marquee-inner">学祭まであと少し〜</div>
+                </div>
+        </div>
+</div>
+
+  <div class="item4">
+        <div class="areasub">
+                <img src="./icon.jpg">&nbsp;もりかぷ @InaSMori
+        </div>
+        <div class="textareayellow">
+                <div class="marquee">
+                        <div class="marquee-inner">hogehoge</div>
+                </div>
+        </div>
+</div>
+
+</div>
+
 </body>
 </html>
-
-
