aff21cb40841d8d9b3b68f980294bbe205e252fc
tmorio <morikapusan@eleken.jp>
2018-10-20 22:54:31 +0900

    Backup

---
+++ front/tate.php
@@ -0,0 +1,82 @@
+<!doctype html>
+
+<html lang="ja">
+<head>
+	<meta charset="utf-8">
+	<title>Design Test</title>
+	<link rel="stylesheet" href="tate.css">
+</head>
+
+<body>
+
+<div class="title">
+	<img border="0" src="./twitterlogo.png">&thinsp;&thinsp;<span class="title">#koryosai2018</span>
+</div>
+
+<br>
+
+<div class="container">
+  <div class="photo">
+	<div class="bigarea">
+		<img src="./pic.jpg" class="bigpic">
+	</div>
+	<div class="area">
+		<img src="./icon.jpg">&nbsp;もりかぷ @InaSMori
+	</div>
+	<br>
+	<div class="textarea">
+		<div class="marquee">
+			<div class="marquee-inner">それっぽくなってきた！</div>
+		</div>
+	</div>
+  </div>
+
+  <div class="item1">
+        <div class="areasub">
+                <img src="./icon.jpg">&nbsp;もりかぷ @InaSMori
+        </div>
+        <div class="textareapink">
+                <div class="marquee">
+                        <div class="marquee-innera">PHPはいいぞ。</div>
+                </div>
+	</div>
+</div>
+
+  <div class="item2">
+        <div class="areasub">
+                <img src="./icon.jpg">&nbsp;もりかぷ @InaSMori
+        </div>
+        <div class="textareagreen">
+                <div class="marquee">
+                        <div class="marquee-innerb">イナズマイレブンのゲームはいつ出るのやら...</div>
+                </div>
+        </div>
+</div>
+
+  <div class="item3">
+        <div class="areasub">
+                <img src="./icon.jpg">&nbsp;もりかぷ @InaSMori
+        </div>
+        <div class="textareablue">
+                <div class="marquee">
+                        <div class="marquee-innerc">学祭まであと少し〜</div>
+                </div>
+        </div>
+</div>
+
+  <div class="item4">
+        <div class="areasub">
+                <img src="./icon.jpg">&nbsp;もりかぷ @InaSMori
+        </div>
+        <div class="textareayellow">
+                <div class="marquee">
+                        <div class="marquee-innerd">hogehoge</div>
+                </div>
+        </div>
+</div>
+
+</div>
+
+</body>
+</html>
+
