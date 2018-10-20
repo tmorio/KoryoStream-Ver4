00f3ca4e24cfd1bf9ef18362062806826e870908
tmorio <morikapusan@eleken.jp>
2018-10-20 13:14:02 +0900

    add front (test)

---
+++ front/index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>Design Test</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<div class="title">
+        #koryosai2018
+</div>
+
+<div class="container">
+  <div class="item box-big1">Content with Media</div>
+  <div class="item1">Content Text1</div>
+  <div class="item2">Content Text2</div>
+  <div class="item3">Content Text3</div>
+  <div class="item4">Content Text4</div>
+</div>
+</body>
+</html>
+
+
