21f2cc19d14a167abe7a2f91863468a859bd0a2c
tmorio <morikapusan@eleken.jp>
2018-10-26 23:12:44 +0900

    HTMLの乱れを調整

--- index.php
+++ index.php
@@ -6,40 +6,42 @@ require_once('./backend/myid.php');
 
 <html lang="ja">
 <head>
-	<meta charset="utf-8">
-	<title>KoryoStream Ver4</title>
-	<link rel="stylesheet" href="styles.css?Ver=4">
+        <meta charset="utf-8">
+        <meta name="author" content="T.Morio">
+        <title>KoryoStream Ver4</title>
+        <link rel="stylesheet" href="styles.css?Ver=4">
+        <link rel="icon" href="favicon.ico">
 </head>
 
 <body>
 <script type="text/javascript" src="prototype.js"></script>
 <script type="text/javascript" src="ajaxupdater.js?=Ver4"></script>
 <div class="title">
-	<img border="0" src="./twitterlogo.png">&thinsp;<span class="title"><?php echo TAGNAME ?></span>
+        <img border="0" src="./twitterlogo.png">&thinsp;<span class="title"><?php echo TAGNAME ?></span>
 </div>
 
 <br>
 
 <div class="container">
-  <div class="photo">
-        <div id="itemphoto"></div>
-  </div>
+        <div class="photo">
+                <div id="itemphoto"></div>
+        </div>
 
-  <div class="item1">
-        <div id="item1"></div>
-</div>
+        <div class="item1">
+                <div id="item1"></div>
+        </div>
 
-  <div class="item2">
-        <div id="item2"></div>
-</div>
+        <div class="item2">
+                <div id="item2"></div>
+        </div>
 
-  <div class="item3">
-        <div id="item3"></div>
-</div>
+        <div class="item3">
+                <div id="item3"></div>
+        </div>
 
-  <div class="item4">
-	<div id="item4"></div>
-</div>
+        <div class="item4">
+                <div id="item4"></div>
+        </div>
 
 </div>
 
